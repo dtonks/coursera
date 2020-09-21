9be00b6b89a7ea9aff984f0747d81a957180c313
Darren Tonks <dtonks33@gmail.com>
2020-09-20 22:04:12 -0700

    simple script to add key to dictionary if it doesnt exist or to increment it by 1

---
+++ processing_log_files/imporving_output.py
@@ -0,0 +1,6 @@
+usernames = {}
+name = 'good_user'
+usernames[name] = usernames.get(name, 0) +1
+print(usernames) # {'good_user': 1}
+usernames[name] = usernames.get(name, 0) +1
+print(usernames) # {'good_user': 2}
