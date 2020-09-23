142cacd144f492424fdd94dc93cff979cfce42cf
Darren Tonks <dtonks33@gmail.com>
2020-09-22 21:27:09 -0700

    basic bash code

---
+++ pipes_pipelines/gather-information.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Starting at: $(date)"
+
+echo "UPTIME"
+uptime
+echo
+
+echo "FREE"
+FREE
+echo
+
+echo "WHO"
+who
+echo
+
+echo "Finishing at: $(date)"
+
+
+# can also write on one line using ;
+# echo "Starting at: $(date)"
+
+# echo "UPTIME"; uptime; echo
+
+# echo "FREE"; FREE; echo
+
+# echo "WHO"; who; echo
+
+# echo "Finishing at: $(date)"
