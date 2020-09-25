b604699a4396646a96f22068eace8c945ea43f91
Darren Tonks <dtonks33@gmail.com>
2020-09-24 21:57:07 -0700

    added regex for error message output

--- final_project/regex_trials.py
+++ final_project/regex_trials.py
@@ -7,15 +7,16 @@ line2 = "May 27 11:45:40 ubuntu.local ticky: ERROR: Error creating ticket [#1234
 print(re.search(r"ticky: ERROR: ([\w ]*) ", line2))
 
 
-"""Now that you know how to use regular expressions with Python, start fetching logs of ticky for a specific username. We'll need them in later sections."""
-
- # count how many errors are of the same type
 error1 = "Jan 31 01:43:10 ubuntu.local ticky: ERROR Tried to add information to closed ticket (jackowens)"
 error2 = "Jan 31 02:55:31 ubuntu.local ticky: ERROR Ticket doesn't exist (xlg)"
 error3 = "Jan 31 03:05:35 ubuntu.local ticky: ERROR Timeout while retrieving information (ahmed.miller)"
 error4 = "Jan 31 08:01:40 ubuntu.local ticky: ERROR Tried to add information to closed ticket (jackowens)"
 
-regex = "\((.*?)\)"
-user = re.search(r"\((.*?)\)", error1)
-print(user[1])
- # how many info and error messages there are for a given user (dictionary)
+# count how many errors are of the same type
+regex_error_msg = r": (.*?) \("
+error_msg = re.search(regex_error_msg, error1)
+print(error_msg[1])
+# how many info and error messages there are for a given user (dictionary)
+regex_username = r"\((.*?)\)"
+username = re.search(regex_username, error1)
+print(username[1])
